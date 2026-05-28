d6c5951f3a27c1e186bd928220f72d0bcc08c198
meolord <shenmuegm@gmail.com>
2026-05-28 19:45:03 +0800

    Skip fleet preparation for hard campaign mode

--- module/map/map_fleet_preparation.py
+++ module/map/map_fleet_preparation.py
@@ -354,7 +354,7 @@ class FleetPreparation(InfoHandler):
         #         submarine.raise_hard_not_satisfied()
 
         # Skip fleet preparation in hard mode
-        self.map_is_hard_mode = h1 or h2 or h3
+        self.map_is_hard_mode = self.config.Campaign_Mode == 'hard' or h1 or h2 or h3
         if self.map_is_hard_mode:
             logger.info('Hard Campaign. No fleet preparation')
             # Clear submarine if user did not set a submarine fleet
